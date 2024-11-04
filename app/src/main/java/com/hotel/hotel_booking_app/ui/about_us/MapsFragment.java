af55e7153e1ac1ae453e9d4ae5040df37cf59179
NghiaHaNgoc <nghiaha.22022003@gmail.com>
2024-11-04 10:46:05 +0700

    Add map

---
+++ app/src/main/java/com/hotel/hotel_booking_app/ui/about_us/MapsFragment.java
@@ -0,0 +1,51 @@
+package com.hotel.hotel_booking_app.ui.about_us;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.hotel.hotel_booking_app.R;
+
+public class MapsFragment extends Fragment {
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng fpt = new LatLng(13.804109268283815, 109.21912414900775);
+            googleMap.addMarker(new MarkerOptions().position(fpt).title(""));
+
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(fpt));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
